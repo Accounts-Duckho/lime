e4141c00b232a5853403d7a29b81931bb1ea5300
unknown <Jungdeokho@gmail.com>
2015-02-04 22:29:55 +0900

    Update Function.java

--- Rhyme/src/design/Function.java
+++ Rhyme/src/design/Function.java
@@ -3,6 +3,7 @@ package design;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
@@ -11,10 +12,29 @@ public class Function {
 	private void load(boolean b) {
 		/* for the test */
 		JPanel func = new JPanel();
-		JButton function = new JButton("Function Area");
-		func.add(function);
-		function.setBackground(Color.WHITE);
-		function.setForeground(Color.BLACK);
+		JButton load_btn = new JButton("+");// load song
+		JButton list_btn = new JButton("L");// play list
+		JButton favorite_btn = new JButton("F");// most_played songs
+		JButton Volume = new JButton("V");// volume control
+		
+		func.setPreferredSize(new Dimension(120, 30));
+		
+		func.add(load_btn);
+		load_btn.setBackground(Color.WHITE);
+		load_btn.setForeground(Color.BLACK);
+		
+		func.add(list_btn);
+		list_btn.setBackground(Color.WHITE);
+		list_btn.setForeground(Color.BLACK);
+		
+		func.add(favorite_btn);
+		favorite_btn.setBackground(Color.WHITE);
+		favorite_btn.setForeground(Color.BLACK);
+		
+		func.add(Volume);
+		Volume.setBackground(Color.WHITE);
+		Volume.setForeground(Color.BLACK);
+		
 		MainFrame.main.add(func, BorderLayout.SOUTH);
 	}
 	public void loadFunc(boolean b) {
