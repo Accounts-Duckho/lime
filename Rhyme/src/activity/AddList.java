62b5546c8ea9cb70f53d99d779516843848e3316
ApinkLove <Jungdeokho@gmail.com>
2015-02-15 19:51:14 +0900

    create add button

---
+++ Rhyme/src/activity/AddList.java
@@ -0,0 +1,12 @@
+package activity;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import design.FunctionDock;
+
+public class AddList implements ActionListener {
+	public void actionPerformed(ActionEvent e) {
+		/* List of songs */
+	}
+}
--- Rhyme/src/design/PlayList.java
+++ Rhyme/src/design/PlayList.java
@@ -9,11 +9,13 @@ import java.util.ArrayList;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
+import activity.AddList;
+
 @SuppressWarnings("serial")
 public class PlayList extends JFrame {
 	private ArrayList<String> playlist;
 	private JButton delete;
-	private JButton plus;
+	private JButton add_btn;
 	public PlayList()
 	{
 		super("Favorite List");
@@ -23,6 +25,9 @@ public class PlayList extends JFrame {
 		playlist=new ArrayList<String>();
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setLayout(new BorderLayout());
+		add_btn = new JButton("add");
+		add_btn.addActionListener(new AddList());
+		add(add_btn, BorderLayout.NORTH);
 	}
 	public void showList() {
 		super.setVisible(true);
